711df7cbdb2af5e8756d66f48e70976eef0a216f
chineaalvarez <alfredochinea@outlook.com>
2025-03-21 22:51:31 +0100

    Remove sleep delay and initiate OSDCloud setup for Windows 11 24H2 Pro

--- OSD/OSDCloud_StartURL_Scripts/VM_W1124h2_Pro.ps1
+++ OSD/OSDCloud_StartURL_Scripts/VM_W1124h2_Pro.ps1
@@ -1,3 +1,3 @@
 Write-Host -ForegroundColor Yellow "Starting VM Setup"
-Start-Sleep -Seconds 10
+
 Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage en-us -OSEdition Pro -OSActivation Retail -Restart -ZTI
