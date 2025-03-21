79b068a756a3848c41a8a23bdfa9edd4ae7873e0
chineaalvarez <alfredochinea@outlook.com>
2025-03-21 22:28:20 +0100

    Add sleep delay before starting OSDCloud for Windows 11 24H2 Pro

--- OSD/OSDCloud StartURL Scripts/VM_W1124h2_Pro.ps1
+++ OSD/OSDCloud StartURL Scripts/VM_W1124h2_Pro.ps1
@@ -1,1 +1,2 @@
-Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage en-us -OSEdition Pro -OSActivation Retail -Restart
+Start-Sleep -Seconds 10
+Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage en-us -OSEdition Pro -OSActivation Retail -Restart -ZTI
